4fcf2b3b3e8d69ca20a57ff158b78622c0d99d1b
moktadir-mirul <moktadir.mirul@gmail.com>
2025-08-10 11:18:09 +0600

    loading added in dash stat

--- src/Pages/DashStats/Maindash.jsx
+++ src/Pages/DashStats/Maindash.jsx
@@ -3,13 +3,19 @@ import { AuthContext } from "../../AuthProvider/AuthContext";
 
 const Maindash = () => {
   const { userInfo } = useContext(AuthContext);
+  const [statLoading, setStatLoading] = useState(true);
+  const [foodsLoading, setFoodsLoading] = useState(true);
   const [allFoods, setAllFoods] = useState();
   const [orders, setOrders] = useState();
 
   useEffect(() => {
     fetch("https://royal-bites-rest-server.vercel.app/foods")
       .then((res) => res.json())
-      .then((data) => setAllFoods(data));
+      .then((data) => {
+        setAllFoods(data)
+        setFoodsLoading(false);
+      }
+      );
 
     fetch(
       `https://royal-bites-rest-server.vercel.app/foodOrders?email=${userInfo.email}`,
@@ -22,18 +28,19 @@ const Maindash = () => {
       .then((res) => res.json())
       .then((data) => {
         setOrders(data);
+        setStatLoading(false);
       });
   }, [userInfo.accessToken, userInfo.email]);
 
 const myAddedFoods = allFoods?.filter(food => food.userEmail === userInfo.email);
   return (
     <div className="p-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Card title="🍔 Total Foods" value={allFoods?.length} />
+      <Card title="🍔 Total Foods" value={foodsLoading ? <span className="loading loading-spinner loading-lg"></span> : allFoods?.length} />
       <Card
         title="🛒 Foods Added by You"
-        value={myAddedFoods?.length}
+        value={statLoading ? <span className="loading loading-spinner loading-lg"></span>: myAddedFoods?.length}
       />
-      <Card title="📦 Food Ordered by You" value={orders?.length} />
+      <Card title="📦 Food Ordered by You" value={statLoading? <span className="loading loading-spinner loading-lg"></span>:orders?.length} />
     </div>
   );
 };
